6ff5be7854df83e243cba88133b5b27f9755f31a
mmedeiro <31313132@mackezista.com.br>
2015-07-08 12:52:36 -0300

    alterando imagem ok 😁

--- UserDefaults/UserDefaults/ViewController.swift
+++ UserDefaults/UserDefaults/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var nomeTextField: UITextField!
     @IBOutlet weak var sobrenomeTextField: UITextField!
@@ -27,8 +27,27 @@ class ViewController: UIViewController {
     }
 
     @IBAction func botaoAlterarImagem(sender: AnyObject) {
+        
+        var picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(picker, animated: true, completion: nil)
     }
 
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        
+        imageView.image = image
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
     @IBAction func botaoSalvar(sender: AnyObject) {
     }
 }
